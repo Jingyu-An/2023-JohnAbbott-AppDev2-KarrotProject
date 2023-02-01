f4270e44c91ce45c7cecafcb9af532263c198774
Jingyu-An <jqrohan@gmail.com>
2023-02-01 14:36:46 -0500

    [Karrot] Add role(Admin, User) and admin account

---
+++ Data/IdentitySeedData.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Karrot.Data;
+
+public class IdentitySeedData
+{
+    public static async Task Initialize(KarrotDbContext context,
+        UserManager<IdentityUser> userManager,
+        RoleManager<IdentityRole> roleManager)
+    {
+        context.Database.EnsureCreated();
+
+        string adminRole = "Admin";
+        string userRole = "User";
+
+        if (await roleManager.FindByNameAsync(adminRole) == null)
+        {
+            await roleManager.CreateAsync(new IdentityRole(adminRole));
+        }
+
+        if (await roleManager.FindByNameAsync(userRole) == null)
+        {
+            await roleManager.CreateAsync(new IdentityRole(userRole));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
-// var connectionString = builder.Configuration.GetConnectionString("KarrotDbContextConnection") ?? throw new InvalidOperationException("Connection string 'KarrotDbContextConnection' not found.");
+    
+//var config = builder.Configuration; //For external provider authentication
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -22,11 +23,38 @@ builder.Services.AddSession();
 builder.Services.AddDefaultIdentity<IdentityUser>(opt =>
 {
     opt.SignIn.RequireConfirmedAccount = false;
+    opt.SignIn.RequireConfirmedPhoneNumber = false;
+    
     opt.Password.RequireDigit = false;
+    opt.Password.RequireLowercase = false;
+    opt.Password.RequireUppercase = false;
+    opt.Password.RequireNonAlphanumeric = false;
+    opt.Password.RequiredLength = 4;
+    
+    // Lockout settings.
+    opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    opt.Lockout.MaxFailedAccessAttempts = 5;
+    opt.Lockout.AllowedForNewUsers = true;
+    
+    // User settings.
+    opt.User.AllowedUserNameCharacters =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
+    opt.User.RequireUniqueEmail = false;
 })
 .AddRoles<IdentityRole>()
 .AddEntityFrameworkStores<KarrotDbContext>()
 .AddDefaultTokenProviders();
+
+
+// TODO: For Google provider authentication
+// builder.Services.AddAuthentication()
+//     .AddGoogle(options =>
+//     {
+//         IConfigurationSection googleAuthNSection =
+//             config.GetSection("Authentication:Google");
+//         options.ClientId = googleAuthNSection["ClientId"];
+//         options.ClientSecret = googleAuthNSection["ClientSecret"];
+//     });
 
 var app = builder.Build();
 
@@ -59,6 +87,20 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope()) {
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<KarrotDbContext>();    
+
+    context.Database.Migrate();
+
+    var userMgr = services.GetRequiredService<UserManager<IdentityUser>>();  
+    var roleMgr = services.GetRequiredService<RoleManager<IdentityRole>>();  
+
+    IdentitySeedData.Initialize(context, userMgr, roleMgr).Wait();
+}
+
 app.MapHub<ChatHub>("chatHub");
 
 app.Run();
