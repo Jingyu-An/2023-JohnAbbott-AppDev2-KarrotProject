1bfba32007b019b092a1a197c97cc5aa29139eb2
Lucie-HJ-ROH <lucie.hj.roh@gmail.com>
2023-01-31 22:34:09 -0500

    Add SignalR library & making initial Chat function

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace Karrot.Hub;
+
+
+public class ChatHub : Microsoft.AspNetCore.SignalR.Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
+
+
---
+++ Pages/SimpleChat.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Karrot.Pages;
+
+public class SimpleChat : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
