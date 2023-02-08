cadd1266b139b5f043be7a226042c8098eec4e43
Pat <la_spirou@hotmail.com>
2023-02-07 23:36:38 -0500

    User's address section

--- Areas/Identity/Pages/Account/Manage/Address.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Address.cshtml.cs
@@ -1,34 +1,89 @@
+
+#nullable disable
+
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using Karrot.Data;
 using Karrot.Models;
-using Microsoft.AspNetCore.Authorization;
+using Karrot.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Karrot.Areas.Identity.Pages.Account.Manage.Addresses
 {
     [Authorize]
-    public class IndexModel : PageModel
+    public class AddressModel : PageModel
     {
-        private readonly KarrotDbContext context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<ChangePasswordModel> _logger;
+        private readonly KarrotDbContext _context;
 
-        public IndexModel(KarrotDbContext context)
+
+        public AddressModel(
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager,
+            KarrotDbContext context,
+        ILogger<ChangePasswordModel> logger)
         {
-            this.context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = logger;
+            _context = context;
+        }
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Required]
+            public string Address { get; set; }
+
+            [Required]
+            public string City { get; set; }
+
+            [Required]
+            public string State { get; set; }
+
+            [Required]
+            public string PostalCode { get; set; }
+
         }
 
-        public IList<Address> Address { get;set; } = default!;
-
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetAsync()
         {
-            if (context.Address != null)
+            var userId = _userManager.GetUserId(User);
+            var address = await _context.Address.FirstOrDefaultAsync(x => x.User.Id == userId);
+
+
+            Input = new InputModel
             {
-                Address = await context.Address.Where(u => u.User.UserName == User.Identity.Name).ToListAsync();
-            }
+                Address = address.AddressLine1,
+                City = address.City,
+                State = address.State,
+                PostalCode = address.PostalCode
+            };
+            return Page();
         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var userId = _userManager.GetUserId(User);
+            var address = await _context.Address.FirstOrDefaultAsync(x => x.User.Id == userId);
+
+            address.AddressLine1 = Input.Address;
+            address.City = Input.City;
+            address.State = Input.State;
+            address.PostalCode = Input.PostalCode;
+
+            await _context.SaveChangesAsync();
+            return Page();
+        }
+
+
     }
 }
